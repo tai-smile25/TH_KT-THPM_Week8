c0ee7f1f8797aea9574ce85635655ef033be667f
taismile25 <121488993+tai-smile25@users.noreply.github.com>
2025-04-19 10:42:57 +0700

    Delete

--- SalesManagement/OrderService/routes/orderRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router(); 
-const orderController = require ('../controller/orderController');
-
-router.post('/createOrder', orderController.createOrder);
-
-module.exports = router;
